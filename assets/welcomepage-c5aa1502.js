eec568f2cd10580d2165105ba6689c0bc2797b76
ongzhili <ongzhili@users.noreply.github.com>
2026-01-16 07:45:20 +0000

    deploy: bb7c30562fd21a889491bc93fd27059a4ca1ee90

---
+++ assets/welcomepage-c5aa1502.js
@@ -0,0 +1,9 @@
+const i=`# #hello-world.md
+
+Hi, you may know me as **Zhi Li (or [Dawnwing](https://osu.ppy.sh/users/5144534)).** As of creating this post, I just finished my 3rd year of schooling in NUS, pursuing a Bachelors in Computer Science.
+
+This website was made to be a hub of sorts, just for me to publish my thoughts, and as a host for various tools and projects that I will work on. So do look forward to some exciting things in the coming years.
+
+"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."`;export{i as default};
